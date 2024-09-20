8e080fdc3b1daa68234ef4fc6b2f60823044f3ea
Daniel Schloms <daniel.schloms@gmail.com>
2024-09-20 13:28:57 +0000

    Add `vrsub`

--- src/target/riscv/softvector.cpp
+++ src/target/riscv/softvector.cpp
@@ -363,6 +363,36 @@ uint8_t vsub_vx(
 	return (0);
 }
 
+uint8_t vrsub_vx(
+	void* pV,
+	void* pR,
+	uint16_t pVTYPE, uint8_t pVm, uint8_t pVd, uint8_t pVs2, uint8_t pRs1, uint16_t pVSTART, uint16_t pVLEN, uint16_t pVL, uint8_t pXLEN) {
+	VTYPE::VTYPE _vt(pVTYPE);
+	uint8_t* ScalarReg;
+	uint8_t* VectorRegField;
+
+	VectorRegField = static_cast<uint8_t*>(pV);
+	if(pXLEN <= 32) ScalarReg = &((static_cast<uint8_t*>(pR))[pRs1*4]);
+	else ScalarReg = &(static_cast<uint8_t*>(pR)[pRs1*8]);
+
+	VARITH_INT::rsub_vx(VectorRegField, _vt._z_lmul, _vt._n_lmul, _vt._sew / 8, pVL, pVLEN / 8, pVd, pVs2, ScalarReg, pVSTART, pVm, pXLEN/8);
+
+	return (0);
+}
+
+uint8_t vrsub_vi(
+	void* pV,
+	uint16_t pVTYPE, uint8_t pVm, uint8_t pVd, uint8_t pVs2, uint8_t pVimm, uint16_t pVSTART, uint16_t pVLEN, uint16_t pVL) {
+	VTYPE::VTYPE _vt(pVTYPE);
+	uint8_t* VectorRegField;
+
+	VectorRegField = static_cast<uint8_t*>(pV);
+
+	VARITH_INT::rsub_vi(VectorRegField, _vt._z_lmul, _vt._n_lmul, _vt._sew / 8, pVL, pVLEN / 8, pVd, pVs2, pVimm, pVSTART, pVm);
+
+	return (0);
+}
+
 uint8_t vwaddu_vv(
 	void* pV,
 	uint16_t pVTYPE, uint8_t pVm, uint8_t pVd, uint8_t pVs1, uint8_t pVs2, uint16_t pVSTART, uint16_t pVLEN, uint16_t pVL) {
